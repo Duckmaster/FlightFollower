f22043fa02ae1ec991f107eae6ffa5c680af1ba4
Duckmaster <hello14145@gmail.com>
2023-02-17 12:22:19 +0000

    Made all vars optional

--- lib/models/flight.dart
+++ lib/models/flight.dart
@@ -1,19 +1,19 @@
 class Flight {
-  final String user;
-  final String organisation;
-  final String aircraftIdentifier;
+  final String? user;
+  final String? organisation;
+  final String? aircraftIdentifier;
   final String? copilot;
-  final String numPersons;
-  final String departureLocation;
-  final String destination;
-  final String departureTime;
-  final String ete;
-  final String endurance;
-  final String monitoringPerson;
-  final String flightType;
+  final String? numPersons;
+  final String? departureLocation;
+  final String? destination;
+  final String? departureTime;
+  final String? ete;
+  final String? endurance;
+  final String? monitoringPerson;
+  final String? flightType;
 
   Flight(
-      this.user,
+      {this.user,
       this.organisation,
       this.aircraftIdentifier,
       this.numPersons,
@@ -24,5 +24,5 @@ class Flight {
       this.endurance,
       this.monitoringPerson,
       this.flightType,
-      {this.copilot});
+      this.copilot});
 }
