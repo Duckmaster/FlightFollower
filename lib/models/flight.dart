3a3a4b72b2823700af324b28647c1c44b07b6374
Duckmaster <hello14145@gmail.com>
2023-02-17 12:30:09 +0000

    Added toFirestore() method

--- lib/models/flight.dart
+++ lib/models/flight.dart
@@ -1,16 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Flight {
-  final String? user;
-  final String? organisation;
-  final String? aircraftIdentifier;
-  final String? copilot;
-  final String? numPersons;
-  final String? departureLocation;
-  final String? destination;
-  final String? departureTime;
-  final String? ete;
-  final String? endurance;
-  final String? monitoringPerson;
-  final String? flightType;
+  String? user;
+  String? organisation;
+  String? aircraftIdentifier;
+  String? copilot;
+  String? numPersons;
+  String? departureLocation;
+  String? destination;
+  String? departureTime;
+  String? ete;
+  String? endurance;
+  String? monitoringPerson;
+  String? flightType;
 
   Flight(
       {this.user,
@@ -25,4 +27,21 @@ class Flight {
       this.monitoringPerson,
       this.flightType,
       this.copilot});
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      "user": user,
+      "organisation": organisation,
+      "aircraft_ident": aircraftIdentifier,
+      "copilot": copilot,
+      "num_persons": numPersons,
+      "departure": departureLocation,
+      "destination": destination,
+      "departure_time": departureTime,
+      "ete": ete,
+      "endurance": endurance,
+      "monitoring_person": monitoringPerson,
+      "flight_type": flightType
+    };
+  }
 }
