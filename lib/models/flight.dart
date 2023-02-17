d1d5f746ffb53052a716308f1925246f11230055
Duckmaster <hello14145@gmail.com>
2023-02-17 12:17:43 +0000

    Added a new Flight modle

---
+++ lib/models/flight.dart
@@ -0,0 +1,28 @@
+class Flight {
+  final String user;
+  final String organisation;
+  final String aircraftIdentifier;
+  final String? copilot;
+  final String numPersons;
+  final String departureLocation;
+  final String destination;
+  final String departureTime;
+  final String ete;
+  final String endurance;
+  final String monitoringPerson;
+  final String flightType;
+
+  Flight(
+      this.user,
+      this.organisation,
+      this.aircraftIdentifier,
+      this.numPersons,
+      this.departureLocation,
+      this.destination,
+      this.departureTime,
+      this.ete,
+      this.endurance,
+      this.monitoringPerson,
+      this.flightType,
+      {this.copilot});
+}
