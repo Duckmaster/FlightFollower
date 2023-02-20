a1ccf128926e4e03e07fe74fe91830411d0cca7a
Duckmaster <hello14145@gmail.com>
2023-02-20 11:56:25 +0000

    Added FlightStatuses enum

--- lib/utilities/utils.dart
+++ lib/utilities/utils.dart
@@ -2,6 +2,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flight_follower/models/user.dart';
 import 'dart:convert';
 
+enum FlightStatuses { requested, notstarted, enroute, nearlyoverdue, overdue }
+
 Future<void> storeObject(Object obj, String name) async {
   final prefs = await SharedPreferences.getInstance();
   String objString = jsonEncode(obj);
