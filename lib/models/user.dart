fd7413e45d794f238b6adecf8a41a5bf799aa18f
Duckmaster <hello14145@gmail.com>
2023-02-20 11:33:00 +0000

    Added to/From JSON functions

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -4,4 +4,13 @@ class User {
   final String phoneNumber;
 
   User(this.username, this.email, this.phoneNumber);
+
+  factory User.fromJson(Map<String, dynamic> parsedJson) {
+    return User(
+        parsedJson['username'], parsedJson['email'], parsedJson['phoneNumber']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {"username": username, "email": email, "phoneNumber": phoneNumber};
+  }
 }
