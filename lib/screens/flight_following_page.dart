e90c055b5e9b333e0cbb39ff6d019501236f69b8
Duckmaster <hello14145@gmail.com>
2023-02-27 17:33:16 +0000

    Refactored to work with new FlightsListener class

--- lib/screens/flight_following_page.dart
+++ lib/screens/flight_following_page.dart
@@ -1,89 +1,19 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flight_follower/widgets/flight_item.dart';
-import 'package:flight_follower/utilities/utils.dart';
-import 'package:flight_follower/models/user.dart';
-import 'package:flight_follower/models/request.dart';
-import 'package:flight_follower/models/flight.dart';
+import 'package:provider/provider.dart';
+import 'package:flight_follower/models/flights_listener.dart';
 
-class FlightFollowingPage extends StatefulWidget {
+class FlightFollowingPage extends StatelessWidget {
   const FlightFollowingPage({super.key});
-
-  @override
-  FlightFollowingPageState createState() {
-    return FlightFollowingPageState();
-  }
-}
-
-class FlightFollowingPageState extends State<FlightFollowingPage> {
-  var flights = []; // this needs storing into shared prefs probably
-  late User user;
-
-  @override
-  void initState() {
-    super.initState();
-    getObject("user_object").then((result) {
-      Map<String, dynamic> userMap = result;
-      setState(() {
-        user = User.fromJson(userMap);
-      });
-
-      listenFlightRequests();
-    });
-  }
-
-  void listenFlightRequests() {
-    FirebaseFirestore db = FirebaseFirestore.instance;
-    db
-        .collection("requests")
-        .withConverter(
-            fromFirestore: Request.fromFirestore,
-            toFirestore: (Request request, _) => request.toFirestore())
-        .where("user_id", isEqualTo: user.email)
-        .snapshots()
-        .listen((event) {
-      for (var change in event.docChanges) {
-        Request request = change.doc.data()!;
-        switch (change.type) {
-          case DocumentChangeType.added:
-            db
-                .collection("flights")
-                .doc(request.flightID)
-                .withConverter(
-                    fromFirestore: Flight.fromFirestore,
-                    toFirestore: (Flight flight, _) => flight.toFirestore())
-                .get()
-                .then((value) {
-              Flight flight = value.data()!;
-              FlightItem flightItem = FlightItem(
-                  flight, request.status, deleteFlightItem, value.id);
-              setState(() {
-                flights.add(flightItem);
-              });
-            });
-            break;
-          case DocumentChangeType.modified:
-            break;
-          case DocumentChangeType.removed:
-            break;
-        }
-      }
-    });
-  }
-
-  void deleteFlightItem(FlightItem item) {
-    setState(() {
-      flights.remove(item);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ListView(
-        children: [for (FlightItem flightItem in flights) flightItem],
-      ),
-    );
+    return Consumer<FlightsListener>(builder: (context, value, child) {
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: [for (FlightItem flightItem in value.items) flightItem],
+        ),
+      );
+    });
   }
 }
