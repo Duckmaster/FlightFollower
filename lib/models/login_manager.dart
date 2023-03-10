7063fbadf403c2310083f06add37299b9459c7dc
Duckmaster <hello14145@gmail.com>
2023-03-10 13:52:08 +0000

    notifyListeners() after _isLoggedIn set, store relevant user data in shared prefs after login

--- lib/models/login_manager.dart
+++ lib/models/login_manager.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flight_follower/models/user_model.dart';
 import 'package:flutter/cupertino.dart';
+
+import '../utilities/utils.dart';
 
 class LoginManager extends ChangeNotifier {
   bool _isLoggedIn;
@@ -29,9 +33,23 @@ class LoginManager extends ChangeNotifier {
       if (user == null) {
         print('User is currently signed out!');
         _isLoggedIn = false;
+        notifyListeners();
       } else {
         print('User is signed in!');
         _isLoggedIn = true;
+        notifyListeners();
+        getObject("user_object").then((result) {
+          Map<String, dynamic> userMap = result;
+          if (UserModel.fromJson(userMap).email != user.email) {
+            FirebaseFirestore db = FirebaseFirestore.instance;
+            db.collection("users").doc(user.email).get().then(
+              (docSnapshot) {
+                storeObject(
+                    UserModel.fromJson(docSnapshot.data()!), "user_object");
+              },
+            );
+          }
+        });
       }
     });
   }
