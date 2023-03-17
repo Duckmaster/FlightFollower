943de0b7822d3067bec8c1053bb2f099f8662352
Duckmaster <hello14145@gmail.com>
2023-03-17 14:40:48 +0000

    Added flightID and requestID as fields

--- lib/models/form_state_manager.dart
+++ lib/models/form_state_manager.dart
@@ -7,8 +7,12 @@ class FormStateManager extends ChangeNotifier {
   Flight flight;
   FlightTimings timings;
   bool isSubmitted;
+  String flightID;
+  String requestID;
   FormStateManager()
       : flight = Flight(),
         timings = FlightTimings(),
-        isSubmitted = false;
+        isSubmitted = false,
+        flightID = "",
+        requestID = "";
 }
