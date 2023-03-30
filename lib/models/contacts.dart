ce4d5e8b94bbfd8caf6398a8712d093902dd6bbd
Duckmaster <hello14145@gmail.com>
2023-03-30 19:49:17 +0100

    Moved some logic from constructor to refreshContacts()

--- lib/models/contacts.dart
+++ lib/models/contacts.dart
@@ -1,6 +1,7 @@
 import 'dart:collection';
 import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flight_follower/models/user_model.dart';
 import 'package:flight_follower/utilities/utils.dart';
@@ -10,11 +11,7 @@ class Contacts extends ChangeNotifier {
   late UserModel _user;
 
   Contacts() {
-    getObject("user_object").then((result) {
-      Map<String, dynamic> userMap = result;
-      _user = UserModel.fromJson(userMap);
-      retrieveContactsFromDatabase();
-    });
+    refreshContacts();
   }
 
   UnmodifiableListView<UserModel> get items => UnmodifiableListView(_contacts);
@@ -22,6 +19,14 @@ class Contacts extends ChangeNotifier {
   void addContact(UserModel newContact) {
     _contacts.add(newContact);
     notifyListeners();
+  }
+
+  void refreshContacts() {
+    getObject("user_object").then((result) {
+      Map<String, dynamic> userMap = result;
+      _user = UserModel.fromJson(userMap);
+      retrieveContactsFromDatabase();
+    });
   }
 
   void retrieveContactsFromDatabase() {
