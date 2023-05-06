b91b97f22f624f238e4d87eef58f1b1c47b7ccc5
Duckmaster <hello14145@gmail.com>
2023-05-06 20:27:41 +0100

    Finished adding remainder of API functions

--- lib/utilities/database_api.dart
+++ lib/utilities/database_api.dart
@@ -5,6 +5,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class DatabaseWrapper {
   static final DatabaseWrapper _databaseWrapper = DatabaseWrapper._internal();
   final FirebaseFirestore _db = FirebaseFirestore.instance;
+  final _subscriptions = List.empty(growable: true);
+
   //DatabaseWrapper() : _db = FirebaseFirestore.instance;
 
   factory DatabaseWrapper() {
@@ -21,10 +23,76 @@ class DatabaseWrapper {
     return ref.id;
   }
 
+  Future<void> addDocumentWithID(
+      String collection, String documentID, Map<String, dynamic> data) async {
+    await _db.collection(collection).doc(documentID).set(data);
+    return;
+  }
+
   /// Updates an existing document within [collection] with an identifier matching [documentID]
   /// [data] should take the form {fieldToUpdate: updatedValue}
   Future<void> updateDocument(
       String collection, String documentID, Map<String, dynamic> data) async {
     return await _db.collection(collection).doc(documentID).update(data);
+  }
+
+  Future<Map<String, dynamic>?> getDocument(
+      String collection, String documentID) async {
+    DocumentSnapshot snapshot =
+        await _db.collection(collection).doc(documentID).get();
+    if (!snapshot.exists) return null;
+    return snapshot.data() as Map<String, dynamic>;
+  }
+
+  Future<List<Map<String, dynamic>>> getDocumentsWhere(
+      String collection, List<List<dynamic>> queries) async {
+    Query ref = _db.collection(collection);
+    for (var query in queries) {
+      switch (query[1]) {
+        case "==":
+          ref = ref.where(query[0], isEqualTo: query[2]);
+          break;
+        case ">":
+          ref = ref.where(query[0], isGreaterThan: query[2]);
+          break;
+      }
+    }
+    var docs = await ref.get();
+    var results = List<Map<String, dynamic>>.empty();
+    for (var doc in docs.docs) {
+      results.add(doc.data() as Map<String, dynamic>);
+    }
+    return results;
+  }
+
+  StreamSubscription addListener(String collection, List<List<dynamic>> queries,
+      Function(QuerySnapshot<Object?> event) callback) {
+    Query ref = _db.collection(collection);
+    for (var query in queries) {
+      switch (query[1]) {
+        case "==":
+          ref = ref.where(query[0], isEqualTo: query[2]);
+          break;
+        case ">":
+          ref = ref.where(query[0], isGreaterThan: query[2]);
+          break;
+      }
+    }
+    StreamSubscription sub = ref.snapshots().listen(callback);
+    _subscriptions.add(sub);
+    return sub;
+  }
+
+  Future<void> removeListener(StreamSubscription toRemove) async {
+    _subscriptions.remove(toRemove);
+    return toRemove.cancel();
+  }
+
+  Future<void> removeAllListeners() async {
+    for (var sub in _subscriptions) {
+      await sub.cancel();
+    }
+    _subscriptions.clear();
+    return;
   }
 }
