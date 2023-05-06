2a65e581a95c43b1bfb1bf71b6b02d87b80455f7
Duckmaster <hello14145@gmail.com>
2023-05-06 20:47:27 +0100

    Removed redundant null-aware operators

--- lib/models/request.dart
+++ lib/models/request.dart
@@ -13,9 +13,9 @@ class Request {
   factory Request.fromMap(
     Map<String, dynamic> data,
   ) {
-    return Request(data?["flight_id"], data?["user_id"],
-        FlightStatuses.values.byName(data?["status"]),
-        timestamp: data?["timestamp"]);
+    return Request(data["flight_id"], data["user_id"],
+        FlightStatuses.values.byName(data["status"]),
+        timestamp: data["timestamp"]);
   }
 
   Map<String, dynamic> toFirestore() {
