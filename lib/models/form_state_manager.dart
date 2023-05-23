e1cdd16a8f079a8cf4423a8aef40529abde262f5
Duckmaster <hello14145@gmail.com>
2023-05-23 21:35:47 +0100

    Flight form log state now persists between app restarts

--- lib/models/form_state_manager.dart
+++ lib/models/form_state_manager.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'package:flight_follower/utilities/utils.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -24,6 +24,45 @@ class FormStateManager {
   }
 
   FormStateManager._internal();
+
+  Future<void> save() {
+    Map<String, dynamic> managerMap = {
+      "flight": flight.toFirestore(),
+      "isSubmitted": isSubmitted,
+      "flightID": flightID,
+      "requestID": requestID,
+      "refreshButtonVisible": refreshButtonVisible,
+      "monPersonSelectEnabled": true
+    };
+    return storeObject(managerMap, "form_state");
+  }
+
+  Future<void> load() async {
+    Map<String, dynamic> storedMap = await getObject("form_state");
+    if (storedMap.isEmpty) return;
+    flight = Flight.fromMap(storedMap["flight"]);
+    isSubmitted = storedMap["isSubmitted"];
+    flightID = storedMap["flightID"];
+    requestID = storedMap["requestID"];
+    monPersonIcon = const Icon(Icons.pending);
+    refreshButtonVisible = storedMap["refreshButtonVisible"];
+    monPersonSelectEnabled = storedMap["monPersonSelectEnabled"];
+
+    return;
+  }
+
+  Future<FormStateManager> reset() async {
+    await storeObject(Map<String, dynamic>(), "form_state");
+    flight = Flight(numPersons: "1", endurance: "0.1", ete: 0.1);
+    isSubmitted = false;
+    flightID = "";
+    requestID = "";
+    monPersonIcon = const Icon(Icons.pending);
+    refreshButtonVisible = false;
+    monPersonSelectEnabled = true;
+
+    return this;
+  }
 
   /*FormStateManager()
       : flight = Flight(numPersons: "1", endurance: "0.1", ete: 0.1),
